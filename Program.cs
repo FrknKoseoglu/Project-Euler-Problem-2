3b99f991d82ce92787938dc8553c1c428c995858
FrknKoseoglu <furkan1771@gmail.com>
2017-11-27 15:25:38 +0300

    ProjectEuler Problem 2 solution in C# Console
    ProjectEuler Problem 2 solution in C# Console

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Project_Euler_Problem_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            long f1 = 1, f2 = 1, f3 = 0, toplam = 0;
+
+            while (f3 < 4000000)
+            { 
+                if (f3 % 2 == 0)
+                    toplam = toplam + f3
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+            System.Console.Write("4 Milyona Kadar çift fibonacci sayıların toplamı : " + toplam);
+            System.Console.WriteLine();
+        }
+    }
+}
